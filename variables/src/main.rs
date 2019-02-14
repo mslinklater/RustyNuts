f770102087c2dbb623b7860d7d1db378ec9bd1a3
Martin Linklater <mslinklater@gmail.com>
2019-02-14 23:19:59 +0000

    added variables

---
+++ variables/src/main.rs
@@ -0,0 +1,29 @@
+#[allow(unused_variables)]
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+}
