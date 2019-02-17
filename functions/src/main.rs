c817495990af8fe778947f32b5c34d4e11ba2087
Martin Linklater <mslinklater@gmail.com>
2019-02-17 22:06:13 +0000

    functions done

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -18,6 +18,13 @@ fn main() {
 		x + 1
 	};
 	println!("The value of y is {}", y);
+
+	let x = five();
+	println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+	5
 }
 
 fn another_function() {
