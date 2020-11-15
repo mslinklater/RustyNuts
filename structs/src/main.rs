d0110ce0998230487d71d700248b6cf7af4b8beb
Martin Linklater <mslinklater@gmail.com>
2020-11-15 21:26:11 +0000

    Added rectangles project

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -9,10 +9,21 @@ struct User {
 fn main() {
     println!("Hello, world!");
 
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
+
+    user1.email = String::from("anotheremail@example.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
